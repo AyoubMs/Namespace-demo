488a7a265af436f6455d5b6388a3988d48780b9a
AyoubMS <chahayb563@gmail.com>
2023-07-05 14:25:06 +0200

    add app

---
+++ ConsoleUI/Calculations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoundationInfo
+{
+    public static class Calculations
+    {
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleUI.Models;
+using FoundationInfo;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PersonModel> people = new List<PersonModel>();
+
+            Console.WriteLine();
+
+            PersonModel person = new PersonModel();
+
+            Calculations.Add(3, 4);
+
+            Console.ReadLine();
+        }
+    }
+}
